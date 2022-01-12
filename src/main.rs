7ee3e45c302716a6c05feb9903be058318255fd0
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-01-13 00:39:24 +0100

    Cleaner implemetation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use actix_web::{web, App, HttpServer, HttpRequest};
+use actix_web::{get, App, HttpServer, HttpResponse, Responder};
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {:?}", req);
-    "Hello world!"
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
 #[actix_web::main]
@@ -12,7 +12,7 @@ async fn main() -> std::io::Result<()> {
     println!("Starting server...");
     HttpServer::new(|| {
         App::new()
-            .service(web::resource("/").to(index))
+            .service(index)
     })
     .bind("0.0.0.0:8000")?
     .run()
