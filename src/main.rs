e7d7c26823c2f280f138821bb9c0a23650e7df0e
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-01-13 01:15:09 +0100

    Solves some linter offenses

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,9 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-
     println!("Starting server...");
-    HttpServer::new(|| {
-        App::new()
-            .service(index)
-    })
-    .bind("0.0.0.0:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
